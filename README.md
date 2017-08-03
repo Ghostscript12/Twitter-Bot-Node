9900204f8241aee39e4de924b4e78a4fa9a424cd
Jay Dickson <jaydickson02@gmail.com>
2017-08-03 12:26:03 +1000

    Commited config, licence and Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitter-Bot-Node
+A twitter bot in node. The bot replies to tweets on the @I_am_Mrbot twitter page with a relevant gif.
---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  consumer_key:         'wdwma4d3pcv0LDBBX8LKWEexP',
+  consumer_secret:      'PxiC6CbJxFE85sFJ80pn8Fzrqn1DKGBx3KWLiyzZhSbfr2Cdpx',
+  access_token:         '789727477425840128-gEfoRKf5eUVORto6M6vrYyNuna39uow',
+  access_token_secret:  '13Rn95xBssRdeFBTZ9z9xuyBaKG53Xfl2Brnjk60zcmLf',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+
+}
